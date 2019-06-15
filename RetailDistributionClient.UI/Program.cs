edc43d29019303a0ebc99b6df78fa0e7f434bc79
Bogdan Petrovici <petrovici.bogdan88@gmail.com>
2019-06-15 16:03:45 +0300

    Added dependency injection to UI project. Added MvvmDialogs library to UI project for decoupling viewmodel from views

---
+++ RetailDistributionClient.UI/Program.cs
@@ -0,0 +1,55 @@
+﻿using MvvmDialogs;
+using RetailDistribution.Client.UI.ViewModels;
+using RetailDistribution.Client.UI.Views;
+using SimpleInjector;
+using System;
+
+namespace RetailDistribution.Client.UI
+{
+	static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			var container = Bootstrap();
+
+			// Any additional other configuration, e.g. of your desired MVVM toolkit.
+
+			RunApplication(container);
+		}
+
+		private static Container Bootstrap()
+		{
+			// Create the container as usual.
+			var container = new Container();
+
+			// Register your types, for instance:
+			//container.Register<IQueryProcessor, QueryProcessor>(Lifestyle.Singleton);
+			//container.Register<IUserContext, WpfUserContext>();
+
+			container.Register<IDialogService>(() => { return new DialogService(); }, Lifestyle.Singleton);
+
+			container.Register<MainViewModel>();
+			container.Register<Main>();
+
+			container.Verify();
+
+			return container;
+		}
+
+		private static void RunApplication(Container container)
+		{
+			try
+			{
+				var app = new App();
+				//app.InitializeComponent();
+				var mainWindow = container.GetInstance<Main>();
+				app.Run(mainWindow);
+			}
+			catch (Exception ex)
+			{
+				//Log the exception and exit
+			}
+		}
+	}
+}
---
+++ RetailDistributionClient.UI/Views/Main.xaml.cs
@@ -0,0 +1,101 @@
+﻿using RetailDistribution.Client.UI.ViewModels;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RetailDistribution.Client.UI.Views
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class Main : Window
+	{
+		private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		private HttpClient client;
+
+		public Main(MainViewModel viewModel)
+		{
+			InitializeComponent();
+			DataContext = viewModel;
+
+			client = new HttpClient();
+			// Update port # in the following line.
+			client.BaseAddress = new Uri(System.Configuration.ConfigurationManager.AppSettings["localServerAddress"]);
+			client.DefaultRequestHeaders.Accept.Clear();
+			client.DefaultRequestHeaders.Accept.Add(
+				new MediaTypeWithQualityHeaderValue("application/json"));
+		}
+
+		private async Task RefreshDistricts(Button btn)
+		{
+			if (btn.IsEnabled)
+			{
+				btn.IsEnabled = false;
+				var mainViewModel = DataContext as MainViewModel;
+				await mainViewModel.RefreshDistricts();
+				btn.IsEnabled = true;
+			}
+		}
+
+		private async void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			MainViewModel.Client = client;
+			await RefreshDistricts(RefreshDistrictsButton);
+		}
+
+		private async void RefreshDistricts_Button_Click(object sender, RoutedEventArgs e)
+		{
+			Button btn = (Button)sender;
+			await RefreshDistricts(btn);
+		}
+
+		private async void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			ListBox listBox = (ListBox)sender;
+			if (listBox.IsEnabled)
+			{
+				listBox.IsEnabled = false;
+				await (DataContext as MainViewModel).GetVendorsAsync(ServicePaths.DistrictsEndpoint);
+				await (DataContext as MainViewModel).GetShopsAsync(ServicePaths.ShopsEndpoint);
+				listBox.IsEnabled = true;
+			}
+		}
+
+		private async void SetPrimaryVendor_Button_Click(object sender, RoutedEventArgs e)
+		{
+			Button btn = (Button)sender;
+			var mainViewModel = DataContext as MainViewModel;
+			if (mainViewModel != null && btn.IsEnabled)
+			{
+				await mainViewModel.SetPrimaryVendorAsync(ServicePaths.DistrictsEndpoint);
+			}
+		}
+
+		private async void AddVendor_Button_Click(object sender, RoutedEventArgs e)
+		{
+			var mainViewModel = DataContext as MainViewModel;
+			await mainViewModel.AddVendor();
+		}
+
+		private async void RemoveVendor_Button_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				Button btn = (Button)sender;
+				var mainViewModel = DataContext as MainViewModel;
+				if (mainViewModel != null && btn.IsEnabled)
+				{
+					await mainViewModel.RemoveVendor(ServicePaths.DistrictsEndpoint);
+				}
+			}
+			catch (InvalidOperationException ex)
+			{
+				log.Error(ex.ToString());
+				MessageBox.Show(ex.Message, "Cannot remove vendor", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+		}
+	}
+}
